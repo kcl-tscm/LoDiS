c649bfd6bb13dcd51ee56eb36f48dc6403e5e36f
JonesRobM <52043020+JonesRobM@users.noreply.github.com>
2019-12-09 11:36:39 +0000

    Update Distances.py

--- LoDiS_CC/Distances.py
+++ LoDiS_CC/Distances.py
@@ -11,10 +11,32 @@ class Distance():
         self.distances = distances
         
         self.species = species
-
-#This function calculates the pairwise distance between all possible atom pairings
+""" Robert
+This class of functions is designed to extract information regarding atomic positions
+for a whole trajectory and generate data regarding spatial distributions of all, homo, and
+hetero combinations.
+"""
         
     def Euc_Dist(Vector):
+        
+        """ Robert
+        Vector: The tensor containing the xyz coordinates of all atoms in a given trajectory.
+        This function will only act on one frame at any given time but can be iteratively 
+        called so at to generate information regarding specific frames of a simulation.
+        
+        This is generally expected to be the output of "read_trajectory" in the 
+        "Movie_Read.py" module which generates the xyz coordinates as well as the 
+        numbered list of elements. These are initially kept separate although there
+        does exist provision to stack these tensors to facilitate the homo/hereo 
+        PDDFs.
+        
+        This function is indiscriminate regarding chemical species. It will calculate 
+        pair-wise distances regardless of chemical species and return a 1D numpy array
+        of all possible pairwise distances which is N!(N-1)!/2
+        So bear in mind that this function scales poorly with system size!
+        """
+        
+        
         Distances=[]; Temp=np.array(np.delete(Vector, (0),axis=1), dtype=np.float64)
         for i in range(len(Vector)-1):
             for j in range(i+1,len(Vector)):
@@ -26,9 +48,25 @@ class Distance():
 
 
 
-#This function calculates distances between homo atom pairings only
     
     def M1_M1(Vector,Species):
+        
+        """ Robert
+        
+        Vector: The tensor containing the xyz coordinates of all atoms in a given trajectory.
+        This function will only act on one frame at any given time but can be iteratively 
+        called so at to generate information regarding specific frames of a simulation.
+        
+        Species: The chemical symbol for the element to be considered. Parameter expected 
+        to be input as such
+        
+        "M1_M1(Positions, Au)" if one were to analyse the xyz trajectory and was interested only 
+        in gold.
+        
+        This function has near identical architecture to the one defined above, "Euc_Dist",
+        with the exception that the function actively searches for similar chemical species
+        from the input trajectory.
+        """
         
         tick=time.time()
         Distances=[]; Temp=np.array(np.delete(Vector, (0),axis=1), dtype=np.float64)
@@ -41,10 +79,19 @@ class Distance():
         print("Time to calculate distances: %.2f [s]" %(toc-tic))
         return Distances
 
-
-#This function calculates distances between hetero atom pairings only.
     
     def M1_M2(Vector):
+        
+        """ Robert
+        
+        Vector: The tensor containing the xyz coordinates of all atoms in a given trajectory.
+        This function will only act on one frame at any given time but can be iteratively 
+        called so at to generate information regarding specific frames of a simulation.
+        
+        Note that no species need to be defined for this function as it is understood that LoDiS
+        only has provision for mono/bimetallic systems (for the time being) although this
+        function could be further generalised (albeit it a potential cost to computation time).
+        """
         
         tick=time.time()
         Distances=[]; Temp=np.array(np.delete(Vector, (0),axis=1), dtype=np.float64)
